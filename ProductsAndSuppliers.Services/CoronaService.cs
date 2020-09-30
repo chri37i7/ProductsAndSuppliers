7a848f8df0e89b04599bc196ea30c540e2009faf
Christian Alexander Heilmann Bennedbæk <chri37i7@edu.campusvejle.dk>
2020-09-30 09:27:44 +0200

    Added webservice and stuff

--- ProductsAndSuppliers.Services/CoronaService.cs
+++ ProductsAndSuppliers.Services/CoronaService.cs
@@ -7,14 +7,23 @@ using System.Threading.Tasks;
 namespace ProductsAndSuppliers.Services
 {
     public class CoronaService : WebServiceBase
-    {
+    {   
         protected const string endpoint = "https://corona.lmao.ninja/v2/countries";
 
-        public virtual async Task<IEnumerable<Corona>> GetAll()
+        public virtual async Task<IEnumerable<Corona>> GetAllAsync()
         {
             string json = await CallWebApiAsync(endpoint);
 
             IEnumerable<Corona> countryData = JsonConvert.DeserializeObject<IEnumerable<Corona>>(json);
+
+            return countryData;
+        }
+
+        public virtual async Task<Corona> GetByNameAsync(string countryName)
+        {
+            string json = await CallWebApiAsync($"{endpoint}/{countryName}");
+
+            Corona countryData = JsonConvert.DeserializeObject<Corona>(json);
 
             return countryData;
         }
---
+++ ProductsAndSuppliers.WebApp/Controllers/CoronaController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ProductsAndSuppliers.Entities;
+using ProductsAndSuppliers.Services;
+
+namespace ProductsAndSuppliers.WebApp.Controllers
+{
+    public class CoronaController : Controller
+    {
+        protected readonly CoronaService service;
+
+        public CoronaController()
+        {
+            service = new CoronaService();
+        }
+
+        // GET: CoronaController
+        public virtual async Task<ActionResult> Index()
+        {
+            //IEnumerable<Corona> countryData = await service.GetAllAsync();
+
+            List<Corona> countryData = new List<Corona>();
+            Corona denmark = await service.GetByNameAsync("Denmark");
+            countryData.Add(denmark);
+
+            return View(countryData);
+        }
+    }
+}
